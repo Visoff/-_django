8d446257e8bae9c4074a62172df8dc94ea17aab0
sashynkaDyrachek <ikalinin667@gmail.com>
2023-05-04 20:31:52 +0600

    123

---
+++ news_blog/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+articles = [
+    {
+        "id":1,
+        "title":"First one",
+        "description":"It works?"
+    },
+    {
+        "id":2,
+        "title":"Second one",
+        "description":"It works!"
+    }
+]
+
+# Create your views here.
+def index(req):
+    return render(req, "news_blog/news_list.html", {"articles":articles})
+
+def get_by_id(req, id):
+    for article in articles:
+        if (article.id != id):
+            continue
+        return render(req, "news_blog/news_article.html", {"article":article})
